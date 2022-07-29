276f22f2d0c1c9bc2b81f6cda31cd8982d3e3e00
Ehmad Saeed <justehmadsaeed@gmail.com>
2022-07-29 18:54:39 +0500

    feat: display data on hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import cloudy_icon from 'assets/icons/cloudy.svg';
+import React, { useContext } from 'react';
+import WeatherContext from 'contexts/weather';
 
 const Hero = () => {
+  const [[todayWeather], cityName] = useContext(WeatherContext);
+  const date = new Date();
   return (
     <div className="hero">
       <h3 className="hero__title">Weather App</h3>
       <div className="headline">
         <h1 className="headline__temp">
-          <span>16</span>&deg;
+          <span>{todayWeather?.main.temp.toFixed()}</span>&deg;
         </h1>
         <div className="headline__city">
-          <h3 className="headline__city-name">London</h3>
-          <p className="headline__city-date">06:09 - Mondon, 9 Sep '19</p>
+          <h3 className="headline__city-name">{cityName}</h3>
+          <p className="headline__city-date">{date.toDateString()}</p>
         </div>
         <div className="mood">
-          <img className="mood__icon" src={cloudy_icon} alt="" />
-          <p className="mood__desc">Cloudy</p>
+          <img
+            className="mood__icon"
+            src={`http://openweathermap.org/img/wn/${todayWeather?.weather[0].icon}@4x.png`}
+            alt="weather icon"
+          />
+          <p className="mood__desc">{todayWeather?.weather[0].description}</p>
         </div>
       </div>
     </div>
