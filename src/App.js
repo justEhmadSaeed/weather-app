0db400c4f188edfa781bca8ccca0356a6175982a
Ehmad Saeed <justehmadsaeed@gmail.com>
2022-07-29 18:54:29 +0500

    feat: integrate context api in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { useEffect, useState } from 'react';
 import Hero from 'components/Hero';
 import 'App.css';
 import Sidebar from 'components/Sidebar';
+import axios from 'axios';
+import WeatherContext from 'contexts/weather';
 
 function App() {
+  const [weatherData, setWeatherData] = useState([]);
+  const [city, setCity] = useState('Lahore');
+  useEffect(() => {
+    const controller = new AbortController();
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/forecast?units=metric&q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`,
+        {
+          signal: controller.signal,
+        }
+      )
+      .then((res) => {
+        setWeatherData(res.data.list);
+      });
+    return () => controller.abort();
+  }, [city]);
   return (
-    <div className="App">
-      <Hero />
-      <Sidebar />
-    </div>
+    <WeatherContext.Provider value={[weatherData, city, setCity]}>
+      <div className="App">
+        <Hero />
+        <Sidebar />
+      </div>
+    </WeatherContext.Provider>
   );
 }
 
